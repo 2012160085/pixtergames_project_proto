adda4ed0a03a83be56d283d1a4a9f0f9466a1650
kibeom <lkbest0085@gmail.com>
2020-11-01 23:55:27 +0900

    first commit

---
+++ Assets/Scripts/CombatCallbacks.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombatCallbacks : MonoBehaviour
+{
+
+    // Delegates
+    public delegate void delBeforeHit();
+    public delegate void delAfterHit();
+    public delegate void delBeforeKnockback();
+    public delegate void delAfterKnockback();
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
