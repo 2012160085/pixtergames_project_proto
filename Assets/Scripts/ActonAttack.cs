bd20034d92581ab9ebaa6068cc9819ad2d794321
kibeom <lkbest0085@gmail.com>
2020-11-03 00:41:51 +0900

    Add Actions

--- Assets/Scripts/ActonAttack.cs
+++ Assets/Scripts/ActonAttack.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class ActonAttack : Action
 {
-    private Dictionary<string, string> attackInfo;
+    private Skill skill;
+    
 
 }
---
+++ Assets/Scripts/Skill.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skill
+{
+    public enum enumSkillCode
+    {
+        NormalMeleeAttack
+    }
+    public enumSkillCode skillCode;
+
+
+}
